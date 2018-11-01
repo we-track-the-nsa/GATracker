ec2115c70f6e151b0e2b241f99f607f5277e0706
VijitSingh97 <vijit.singh@mavs.uta.edu>
2018-11-01 11:56:18 -0500

    added create user, and removed storing email:pass in login file in android internal storage

--- app/src/main/java/com/wetrackthensa/chimmy/gatracker/CreateAccount.java
+++ app/src/main/java/com/wetrackthensa/chimmy/gatracker/CreateAccount.java
@@ -1,17 +1,25 @@
 package com.wetrackthensa.chimmy.gatracker;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class CreateAccount extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     private EditText FirstName;
     private EditText LastName;
@@ -21,28 +29,68 @@ public class CreateAccount extends AppCompatActivity {
     private TextView LoginLink;
     private Intent intent;
 
+    private void createUser(String email, String pass){
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser() != null ){
+            intent = new Intent(CreateAccount.this,MainActivity.class);
+            startActivity(intent);
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("CREATEUSER", "createUserWithEmail:success");
+                            intent = new Intent(CreateAccount.this,Login.class);
+                            startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("CREATEUSER", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(CreateAccount.this, "Creation failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
 
-        FirstName = findViewById(R.id.etFirstName);
-        LastName = findViewById(R.id.etLastName);
         Email = findViewById(R.id.etEmail);
+        Password = findViewById(R.id.etPassword);
+
         CreateBtn = findViewById(R.id.btnCreate);
-        Password = findViewById(R.id.caPassword);
         LoginLink = findViewById(R.id.tvLoginLink);
 
-        LoginLink.setOnClickListener(new View.OnClickListener() {
+        CreateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 String email = Email.getText().toString();
                 String pass = Password.getText().toString();
 
+                createUser(email, pass);
+                }
+        });
+
+        LoginLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
                 intent = new Intent(CreateAccount.this,Login.class);
                 startActivity(intent);
             }
         });
+
     }
 }
--- app/src/main/java/com/wetrackthensa/chimmy/gatracker/Login.java
+++ app/src/main/java/com/wetrackthensa/chimmy/gatracker/Login.java
@@ -33,90 +33,52 @@ public class Login extends AppCompatActivity {
     private Button Login;
     private TextView CreateLink;
 
-    static final int READ_BLOCK_SIZE = 100;
-
-    private void storeFile(String user, String pass){
-        String fileName = "login";
-        String fileContents = user + ":" + pass;
-
-        try {
-            FileOutputStream fileout = openFileOutput(fileName, MODE_PRIVATE);
-            OutputStreamWriter outputWriter=new OutputStreamWriter(fileout);
-            outputWriter.write(fileContents);
-            outputWriter.close();
-
-            //display file saved message
-            Toast.makeText(getBaseContext(), "File saved successfully!",
-                    Toast.LENGTH_SHORT).show();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private String readFile(){
-        //reading text from file
-        try {
-            FileInputStream fileIn=openFileInput("login");
-            InputStreamReader InputRead= new InputStreamReader(fileIn);
-
-            char[] inputBuffer= new char[READ_BLOCK_SIZE];
-            String s="";
-            int charRead;
-
-            while ((charRead = InputRead.read(inputBuffer))>0) {
-                // char to string conversion
-                String readstring = String.copyValueOf(inputBuffer,0,charRead);
-                s +=readstring;
-            }
-            InputRead.close();
-            return s;
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private void validate(){
-
-        // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
+    private void validate(String email, String pass){
 
         if (mAuth.getCurrentUser() != null ){
             intent = new Intent(Login.this,MainActivity.class);
             startActivity(intent);
         }
 
-        String login = readFile();
-        if(login != null) {
-            String[] values = login.split(":");
-            mAuth.signInWithEmailAndPassword(values[0], values[1])
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d("LOGIN", "signInWithEmail:success");
-                            intent = new Intent(Login.this,MainActivity.class);
-                            startActivity(intent);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w("LOGIN", "signInWithEmail:failure", task.getException());
-                            Toast.makeText(Login.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-
-                        // ...
+        mAuth.signInWithEmailAndPassword(email, pass)
+            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("LOGIN", "signInWithEmail:success");
+                        intent = new Intent(Login.this,MainActivity.class);
+                        startActivity(intent);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("LOGIN", "signInWithEmail:failure", task.getException());
+                        Toast.makeText(Login.this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
                     }
-                });
+
+                    // ...
+                }
+            });
+    }
+
+    // validate with no toast
+    private void stealthValidate(){
+
+        if (mAuth.getCurrentUser() != null ){
+            intent = new Intent(Login.this,MainActivity.class);
+            startActivity(intent);
         }
-        
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        // REMOVE sign out on start REMOVE
+        mAuth.signOut();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
@@ -125,14 +87,15 @@ public class Login extends AppCompatActivity {
         Login = findViewById(R.id.btnLogin);
         CreateLink = findViewById(R.id.tvCreateLink);
 
+        stealthValidate();
+
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String user = Username.getText().toString();
                 String pass = Password.getText().toString();
 
-                storeFile(user, pass);
-                validate();
+                validate(user,pass);
             }
         });
         CreateLink.setOnClickListener(new View.OnClickListener() {
